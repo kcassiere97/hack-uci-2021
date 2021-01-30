029b7557be882875bd479936e3a7150cea77b92f
Mass Kizar Cassiere <kcassiere97@csu.fullerton.edu>
2021-01-29 22:01:54 -0800

    first commit

---
+++ avalon/avalon/avalonApp.swift
@@ -0,0 +1,17 @@
+//
+//  avalonApp.swift
+//  avalon
+//
+//  Created by Kizar Cassiere on 1/29/21.
+//
+
+import SwiftUI
+
+@main
+struct avalonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
